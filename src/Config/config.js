e7b95534e4744e21e70063f4cc5cafbaa44f9a80
Sara Kfouri <sakfouri@gmail.com>
2018-11-13 09:22:04 +0200

    Delete config.js

--- src/Config/config.js
+++
@@ -1,8 +0,0 @@
-
-export const DB_CONFIG={
-    apiKey: "AIzaSyCG3j6DNaPPxq9IsCKpM5kuBu-COAFDsjI",
-    authDomain: "fir-for-testing-1e749.firebaseapp.com",
-    databaseURL: "https://fir-for-testing-1e749.firebaseio.com/",
-    storageBucket: "fir-for-testing-1e749.appspot.com",
-    
-};
